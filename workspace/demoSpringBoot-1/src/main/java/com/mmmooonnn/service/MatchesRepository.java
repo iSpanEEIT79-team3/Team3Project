a61b05cecd32f3848a16823bbbfa2b3df3dac973
edison <h0962028238@gmail.com>
2024-04-29 15:49:28 +0800

    OKOKKOK

---
+++ workspace/demoSpringBoot-1/src/main/java/com/mmmooonnn/service/MatchesRepository.java
@@ -0,0 +1,7 @@
+package com.mmmooonnn.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatchesRepository extends JpaRepository<MatchesBean, Integer> {
+    
+}
