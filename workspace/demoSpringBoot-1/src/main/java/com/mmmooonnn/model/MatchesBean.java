421b51dce7cbe6ec6fce3147cc785b02499f0f59
mmmooonnn <mhou6vm0@gmail.com>
2024-04-30 12:51:53 +0800

    增加無參建構子

--- workspace/demoSpringBoot-1/src/main/java/com/mmmooonnn/model/MatchesBean.java
+++ workspace/demoSpringBoot-1/src/main/java/com/mmmooonnn/model/MatchesBean.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import jakarta.persistence.Table;
 
 @Entity
@@ -94,5 +93,10 @@ public class MatchesBean implements Serializable {
 		this.matchdate = matchdate;
 		this.matchsuccess = matchsuccess;
 	}
+
+	public MatchesBean() {
+		super();
+	}
+	
 
 }
