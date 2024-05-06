c78070e8e40371ce72569594072cdd05ca511274
edison <h0962028238@gmail.com>
2024-05-06 17:44:47 +0800

    新增一些奇奇怪怪的東西

--- workspace/demoSpringBoot-1/src/main/java/com/mmmooonnn/service/MatchesService.java
+++ workspace/demoSpringBoot-1/src/main/java/com/mmmooonnn/service/MatchesService.java
@@ -7,15 +7,20 @@ import org.springframework.stereotype.Service;
 
 import com.mmmooonnn.model.MatchUserDetailsDTO;
 import com.mmmooonnn.model.MatchesRepository;
+import com.mmmooonnn.model.UsersBeanNew;
+import com.mmmooonnn.model.UsersRepository;
 
 @Service
 public class MatchesService {
 
 	@Autowired
 	private MatchesRepository matchesRepository;
+	
+	@Autowired
+	private UsersRepository usersRepository;
 
-	public List<MatchUserDetailsDTO> getMatchesByStatus(String status) {
-		return matchesRepository.findMatchesByStatus(status);
+	public List<UsersBeanNew> getMatchesByStatus(Integer userid) {
+		return usersRepository.findMatchesByStatus(userid);
 	}
 
 }
