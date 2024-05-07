b827281bd1e5d05c7700fac2b45a074c6be838f8
chienchien6 <128012103+chienchien6@users.noreply.github.com>
2024-05-07 16:12:06 +0800

    time

--- 千期末專題.sql
+++ 千期末專題.sql
@@ -21,17 +21,19 @@ CREATE TABLE Photos (
 
 );
 
-insert into EVENT values('Charleston Movement','2024-05-06','2024-05-16','2024-06-15','2024-06-17','Concert with THREE COOL CATS',
+insert into EVENT values('查爾斯頓搬到台灣','2024-05-06','2024-05-16','2024-06-15','2024-06-17','Concert with THREE COOL CATS',
 'LINDY','400','Asi幸福站','JFSwing','/images/bubble.jpg');
-insert into EVENT values('Jazz Feet Weekend','2024-06-01','2024-06-20','2024-08-08','2024-08-11','Nathan&Meti&Andante',
+insert into EVENT values('爵士腳週末','2024-06-01','2024-06-20','2024-08-08','2024-08-11','Nathan&Meti&Andante',
 'LINDY','700','西門紅樓','JFSwing','/images/logo.jpg');
-insert into EVENT values('Swing Rhythms','2024-07-01','2024-07-18','2024-09-24','2024-09-26','Program:BALBOA workshop with MELANIE & ALBERT',
+insert into EVENT values('Swing節奏','2024-07-01','2024-07-18','2024-09-24','2024-09-26','Program:BALBOA workshop with MELANIE & ALBERT',
 'BALBOA','500','Big Apple studio 復興教室','Big Apple','/images/nathan.jpg');
-insert into EVENT values('Keep Punching','2024-08-01','2024-08-20','2024-08-27','2024-08-30','Herrang Dance Camp',
+insert into EVENT values('SwingFever搖擺舞派對','2024-05-06','2024-05-16','2024-06-15','2024-06-17','歡迎加入我們舉辦的 Swing Fever 搖擺舞派對！這是一個讓您盡情跳躍、歡笑和享受音樂的絕佳機會。我們將在 City Dance Studio 舉辦這個活動，活動期間將會有專業的搖擺舞老師為您帶來精彩的教學課程，同時也會有DJ為大家播放熱情歡快的 Swing 音樂。無論您是初學者還是搖擺舞高手，這都是一個與朋友們一起跳舞、放鬆身心的絕佳之夜。不要錯過這個難得的機會，穿上您的最佳舞鞋，加入我們一起搖擺吧！',
+'LINDY','400','Asi幸福站','JFSwing','/images/bubble.jpg');
+insert into EVENT values('KeepPunching','2024-08-01','2024-08-20','2024-08-27','2024-08-30','Herrang Dance Camp',
 'LINDY','800','shita空間','JFSwing','/images/andante.jpg');
-insert into EVENT values('Live Band Foreast','2024-09-06 10:00:00','2024-09-25 12:00:00','2024-10-15 20:00:00','2024-10-17 23:00:00','Jhouse,Hopshbam DELUXE',
+insert into EVENT values('現場樂隊森林','2024-09-06 10:00:00','2024-09-25 12:00:00','2024-10-15 20:00:00','2024-10-17 23:00:00','Jhouse,Hopshbam DELUXE',
 'LINDY','500','中山堂','JFSwing','/images/party.jpg');
-insert into EVENT values('Live Band Foreast','2024-9-18 10:00:00','2024-09-28 12:00:00','2024-10-09 10:00:00','2024-10-11 12:00:00','Spring Hop',
+insert into EVENT values('夏日搖擺帝國','2024-9-18 10:00:00','2024-09-28 12:00:00','2024-10-09 10:00:00','2024-10-11 12:00:00','Spring Hop',
 'LINDY','600','中山堂','JFSwing','/images/catscorner.jpg');
 
 select*from EVENT;
